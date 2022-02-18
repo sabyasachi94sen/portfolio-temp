9a42fb4727193ac310bf327e309dfceabe65d875
sabyasachi94sen <64438737+sabyasachi94sen@users.noreply.github.com>
2022-02-18 19:07:44 +0530

    uploaded

---
+++ port-folio.js
@@ -0,0 +1,11 @@
+
+$('.toggle-btn').click(function(){
+    $('.navbar').removeClass("close");
+    $('.navbar').addClass("active");
+    
+});
+
+$('.cancel').click(function(){
+    $('.navbar').addClass("close");
+   
+});
